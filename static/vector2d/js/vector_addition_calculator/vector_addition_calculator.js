5994e32f12207a42325804da2370d3dffdb9c5cd
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2023-09-21 05:47:23 -0400

    Refactored code

--- static/vector2d/js/vector_addition_calculator/vector_addition_calculator.js
+++ static/vector2d/js/vector_addition_calculator/vector_addition_calculator.js
@@ -1,4 +1,9 @@
-console.log("Vector Calculator");
+let rows = document.getElementsByClassName("table-row");
+let vector_table = document.getElementById("vector-table");
+let showMath = document.getElementById("show-math");
+let stepsDiv = document.getElementById("steps-div");
+
+let bottomRow = rows[rows.length - 1];
 
 const translations = {
     en: {
@@ -8,9 +13,7 @@ const translations = {
     }
 };
 
-let rows = document.getElementsByClassName("table-row");
-let bottomRow = rows[rows.length - 1];
-bottomRow.addEventListener("keyup", addRow);
+
 
 
 function clamp_n180_180(degrees) {
@@ -24,12 +27,13 @@ function clamp_n180_180(degrees) {
 
 
 function addRow(event) {
-    let inputs = bottomRow.children; // actually the td wrappers for the inputs
-    if (inputs[0].children[0].value !== "" || inputs[1].children[0].value !== "") { // check if the bottom row is not empty
+    let inputs = bottomRow.children; // The actual td wrappers for the inputs
+
+    // check if the bottom row is not empty
+    if (inputs[0].children[0].value !== "" || inputs[1].children[0].value !== "") {
         let row = document.createElement("tr");
         row.setAttribute("class", "table-row");
 
-        // there has to be a better way to do this
         let box1 = document.createElement("td");
         box1.setAttribute("class", "table-box");
         let inp1 = document.createElement("input");
@@ -37,18 +41,18 @@ function addRow(event) {
         inp1.setAttribute("placeholder", translations.en.magnitude);
         box1.append(inp1);
         
-        // but I don't know one
         let box2 = document.createElement("td");
         box2.setAttribute("class", "table-box");
+
         let inp2 = document.createElement("input");
         inp2.setAttribute("class", "table-input");
         inp2.setAttribute("placeholder", translations.en.direction);
         box2.append(inp2);
 
         row.append(box1, box2);
-        document.getElementById("vector-table").children[0].appendChild(row);
+        vector_table.children[0].appendChild(row);
 
-        // update bottomRow and move the event listener onto it
+        // Update bottomRow and move the event listener onto it
         bottomRow.removeEventListener("keyup", addRow);
         rows = document.getElementsByClassName("table-row");
         bottomRow = rows[rows.length - 1];
@@ -64,12 +68,10 @@ function addStep(parent, content) {
 }
 
 
-// I wish js had explicit typing so I could specify this function takes multiple vectors (i.e., vector: [i32; 2])
 function sumVectors(args) {
     let xTotal = 0;
     let yTotal = 0;
     let newX, newY;
-    let stepsDiv = document.getElementById("steps-div");
     stepsDiv.style.borderStyle = "solid";
 
     for (let i = 0; i < args.length; i++) {
@@ -77,7 +79,8 @@ function sumVectors(args) {
         addStep(stepsDiv, `Converting vector: ${vector}`);
 
 
-        if (vector[1] === 0) { // speed up the math for 0 degree cases to skip multiplying by 0 a lot
+        // Speed up the math for 0 degree cases to skip multiplying by 0 a lot
+        if (vector[1] === 0) {
             addStep(stepsDiv, `Finished: (${vector[0]}, 0)`);
             xTotal += vector[0];
         } 
@@ -102,22 +105,24 @@ function sumVectors(args) {
 
     let quad = 1;
     /* quadrant II:
-        resulting angle (finalDir) will be negative because its the arctan of a negative number
+        resulting angle (finalDir) will be negative because it's the arctan of a negative number
         the angle relative to positive x-axis would be the angle adjacent to finalDir
         180 - abs(finalDir), or 180 + finalDir, is the angle in relation to the positive section of the x-axis
      or quad III:
-         finalDir will be positive because its the arctan of a positive number
+         finalDir will be positive because it's the arctan of a positive number
          the correct angle will be finalDir + 180
     */ 
-    if (xTotal < 0)
-    { finalDir += 180; quad = yTotal < 0 ? 3 : 2; }
+    if (xTotal < 0) {
+        finalDir += 180; quad = yTotal < 0 ? 3 : 2;
+    }
     /* quadrant IV:
         finalDir will be negative
         because its in quadrant IV the angle measure relative to the x-axis in quad I ==
         360 - abs(finalDir) == 360 + finalDir
     */
-    else if (yTotal < 0)
-    { finalDir += 360; quad = 4; }
+    else if (yTotal < 0) {
+        finalDir += 360; quad = 4;
+    }
 
     if (quad !== 1)
     {
@@ -129,12 +134,11 @@ function sumVectors(args) {
 }
 
 
-document.getElementById("calculate-button").addEventListener("click", calculate);
 
 function calculate(event) {
     if (event.button === 0) {
-        document.getElementById("steps-div").innerText = "";
-        document.getElementById("steps-div").style.borderStyle = "hidden";
+        stepsDiv.innerText = "";
+        stepsDiv.style.borderStyle = "hidden";
 
         let vectorArray = [];
 
@@ -151,39 +155,54 @@ function calculate(event) {
         }
 
         let outputDiv = document.getElementById("output-div");
+
         if (vectorArray.length < 1) {
-            outputDiv.setAttribute("style", "color: red"); outputDiv.innerText = translations.en.no_vectors;
+            outputDiv.setAttribute("style", "color: red");
+            outputDiv.innerText = translations.en.no_vectors;
         }
         else {
-            outputDiv.setAttribute("style", "color: black"); outputDiv.innerText = "";
+            outputDiv.setAttribute("style", "color: black");
+            outputDiv.innerText = "";
+
             let answer = sumVectors(vectorArray);
+
             let magDiv = document.createElement("div"); magDiv.innerText = "Magnitude: " + answer[0];
-            let dirDiv = document.createElement("div"); dirDiv.innerText = "Direction: " + answer[1] + "°";
-            let dirClampedDiv = document.createElement("div"); dirClampedDiv.innerText = "Direction (-180 to 180): " + clamp_n180_180(answer[1]) + "°";
             outputDiv.prepend(magDiv);
+
+            let dirDiv = document.createElement("div"); dirDiv.innerText = "Direction: " + answer[1] + "°";
             outputDiv.append(dirDiv);
+
+            let dirClampedDiv = document.createElement("div"); dirClampedDiv.innerText = "Direction (-180 to 180): " + clamp_n180_180(answer[1]) + "°";
             outputDiv.append(dirClampedDiv);
         }
     }
 }
 
-document.getElementById("clear-button").addEventListener("click", clearTable);
 
 function clearTable(event) {
-    document.getElementById("steps-div").innerText = "";
-    document.getElementById("steps-div").style.borderStyle = "hidden";
+    stepsDiv.innerText = "";
+    stepsDiv.style.borderStyle = "hidden";
 
-    document.getElementById("vector-table").children[0].remove();
+    vector_table.children[0].remove();
     let tBody = document.createElement("tbody");
 
-    let tRow = document.createElement("tr"); tRow.setAttribute("class", "table-row");
+    let tRow = document.createElement("tr");
+    tRow.setAttribute("class", "table-row");
 
-    let tBox1 = document.createElement("td"); tBox1.setAttribute("class", "table-box");
-    let inp1 = document.createElement("input"); inp1.setAttribute("class", "table-input"); inp1.setAttribute("placeholder", translations.en.magnitude);
+    let tBox1 = document.createElement("td");
+    tBox1.setAttribute("class", "table-box");
+
+    let inp1 = document.createElement("input");
+    inp1.setAttribute("class", "table-input");
+    inp1.setAttribute("placeholder", translations.en.magnitude);
     tBox1.append(inp1);
 
-    let tBox2 = document.createElement("td"); tBox2.setAttribute("class", "table-box");
-    let inp2 = document.createElement("input"); inp2.setAttribute("class", "table-input"); inp2.setAttribute("placeholder", translations.en.direction);
+    let tBox2 = document.createElement("td");
+    tBox2.setAttribute("class", "table-box");
+
+    let inp2 = document.createElement("input");
+    inp2.setAttribute("class", "table-input");
+    inp2.setAttribute("placeholder", translations.en.direction);
     tBox2.append(inp2);
 
     tRow.append(tBox1, tBox2);
@@ -191,17 +210,12 @@ function clearTable(event) {
     tRow.addEventListener("keyup", addRow);
     
     tBody.append(tRow);
-    document.getElementById("vector-table").append(tBody);
+    vector_table.append(tBody);
 }
-
-
-let showMath = document.getElementById("show-math");
-showMath.addEventListener("click", enableMathDisplay);
 
 function enableMathDisplay(event) {
     if (event.button === 0) {
-        let stepsDiv = document.getElementById("steps-div");
-        if (showMath.getAttribute("data-active") === "true") { 
+        if (showMath.getAttribute("data-active") === "true") {
             showMath.setAttribute("style", "background-color: lightgray");
             showMath.setAttribute("data-active", "false");
             stepsDiv.setAttribute("data-active", "false");
@@ -213,3 +227,11 @@ function enableMathDisplay(event) {
         }
     }
 }
+
+
+// Register event listeners
+showMath.addEventListener("click", enableMathDisplay);
+
+document.getElementById("clear-button").addEventListener("click", clearTable);
+document.getElementById("calculate-button").addEventListener("click", calculate);
+bottomRow.addEventListener("keyup", addRow);
